43a3fffd4176e6ed7f4eba90d232be613a2fea31
Mustafa3011 <officewalamustafa@gmail.com>
2021-10-02 21:07:07 +0530

    feat: Login Functionality added

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-import requests
-from flask import Flask, request, jsonify
-
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
+s = os.environ.get("DATABASE_URL")
+s = 'postgresql+psycopg2:/' + s[s.find('/')+1:]
+app.config['SQLALCHEMY_DATABASE_URI'] = s
+db = SQLAlchemy(app)
+
+from db.tables import user
 
 @app.route("/prediction", methods=["POST"])
 def prediction():
@@ -10,3 +16,9 @@ def prediction():
 @app.route("/image_ocr", methods=["POST"])
 def ocr_enable():
     return
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json["Username"]
+    password = request.json["Password"]
+    return dict({"Successful":user.User.login_user(username,password)})
